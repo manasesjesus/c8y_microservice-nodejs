195d949ddb04eb64d3d222017bb9338b6ea6892d
Manasés Jesús <contact@manasesjesus.com>
2019-05-22 17:13:49 +0200

    adding environment endpoint

--- docker/routes.js
+++ docker/routes.js
@@ -14,7 +14,14 @@ module.exports = function(app) {
 
     // Environment variables
     app.route('/environment').get(function(req, res) {
-        res.json({ "status" : "TODO" });
+        res.json({ 
+            "user" : process.env.USER,
+            "platformUrl" : process.env.C8Y_BASEURL, 
+            "microserviceIsolation" : process.env.C8Y_MICROSERVICE_ISOLATION,
+            "tenant" : process.env.C8Y_BOOTSTRAP_TENANT,
+            "bootstrapUser" : process.env.C8Y_BOOTSTRAP_USER,
+            "bootstrapPassword" : process.env.C8Y_BOOTSTRAP_PASSWORD
+        });
     });
 
 };
